6da05937c2181ae58415f6227c0875af948a8793
zequi-pv <72413070+zequi-pv@users.noreply.github.com>
2023-09-15 12:55:39 -0300

    Add files via upload

---
+++ PongSigil/Ejemplo/Ejemplo/main.cpp
@@ -0,0 +1,28 @@
+﻿#include "sl.h"
+
+int main()
+{
+	const int width = 400;
+	const int height = 400;
+
+	slWindow(width, height, "Simple SIGIL Example", false);
+
+	while (!slShouldClose() && !slGetKey(SL_KEY_ESCAPE))
+	{
+		slSetBackColor(0.0, 0.600, 0.0);
+
+		slSetForeColor(1, 1, 1, 1);
+		slSetFont(slLoadFont("fonts/arial.ttf"), 20);
+		slSetTextAlign(SL_ALIGN_CENTER);
+		slSetFontSize(20);
+		slText(200,200,"Hola");
+		//slSetForeColor(1, 1, 1, 1);
+		//slRectangleFill(width * 0.5, height * 0.5, 100, 100);
+
+		slRender();
+	}
+
+	slClose();
+
+	return 0;
+}
